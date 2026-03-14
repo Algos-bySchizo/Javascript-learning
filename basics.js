b3d3f06568ee001ef29cbb2d09e98ca5e3832793
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2026-03-14 10:21:52 +0500

    initializing for Github

---
+++ basics.js
@@ -0,0 +1,20 @@
+//Variable Declaration 
+
+// let score=10;
+// console.log(score)
+// score = 20;
+// console.log(score)
+
+// Use Of + , += and concat()
+
+let greetings = 'hello';
+greetings+=', John';
+console.log(greetings)
+
+let greet='hello';
+let fname='usman';
+console.log(greet+fname)
+
+let str1='Hello';
+let str2='World';
+console.log(str1.concat(' ',str2))
